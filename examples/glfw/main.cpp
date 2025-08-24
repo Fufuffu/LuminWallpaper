650c7be3c60483d8d384a8ff4d57c3b054c7ec0e
samisalami <samgus05@gmail.com>
2025-08-24 22:02:45 +0200

    Comment main.cpp and remove unnecessary includes

--- examples/glfw/main.cpp
+++ examples/glfw/main.cpp
@@ -1,10 +1,6 @@
-#include <Windows.h>
-
-#include <GLFW/glfw3.h>
 #define GLFW_EXPOSE_NATIVE_WIN32
+#include <GLFW/glfw3.h>
 #include <GLFW/glfw3native.h>
-
-#pragma comment(lib, "opengl32.lib")
 
 #include <lumin.h>
 
@@ -14,36 +10,57 @@
 
 int main()
 {
+	// Initializes desktop replacement magic.
 	lumin::Initialize();
 
-	lumin::MonitorInfo monitor = lumin::GetWallpaperTarget(-1);
+	// Sets up the desktop. (-1 is the entire desktop spanning all monitors)
+	lumin::MonitorInfo monitorInfo = lumin::GetWallpaperTarget(-1);
 
+	// Initialize GLFW
 	if (!glfwInit()) {
 		std::cout << "Failed to initialize GLFW" << std::endl;
 		return -1;
 	}
 
-	GLFWwindow *window = glfwCreateWindow(monitor.width, monitor.height, "OpenGL Wallpaper", nullptr, nullptr);
-
+	// Create GLFW window with the size of the monitor.
+	GLFWwindow *window = glfwCreateWindow(
+		monitorInfo.width, monitorInfo.height, "GLFW Wallpaper", nullptr, nullptr
+	);
 	if (!window) {
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
 
+	// Make the window's context current.
 	glfwMakeContextCurrent(window);
 
+	// Retrieve the handle for the GLFW-created window and configure it with lumin.
 	HWND hwnd = glfwGetWin32Window(window);
-	lumin::ConfigureWallpaperWindow(hwnd, monitor);
+	lumin::ConfigureWallpaperWindow(hwnd, monitorInfo);
 
+	// Enable vsync. (caps fps to monitor refresh rate)
+	glfwSwapInterval(1);
+
+	// Main render loop.
 	while (!glfwWindowShouldClose(window)) {
+		// Update the mouse state of the replacement api.
 		lumin::UpdateMouseState();
 
-		if (lumin::IsMonitorOccluded(monitor, 0.95) || lumin::IsDesktopLocked()) {
+		// Skip rendering if the wallpaper is occluded more than 95%.
+		if (lumin::IsMonitorOccluded(monitorInfo, 0.95)) {
 			std::this_thread::sleep_for(std::chrono::milliseconds(100));
 			continue;
 		}
 
+		// Skip rendering if the desktop is locked.
+		// This is useful to avoid unnecessary rendering when the user is not interacting with the desktop.
+		if (lumin::IsDesktopLocked()) {
+			std::this_thread::sleep_for(std::chrono::milliseconds(100));
+			continue;
+		}
+
+		// Clear the screen with a dark blue color.
 		glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
@@ -51,12 +68,14 @@ int main()
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
-
-		std::this_thread::sleep_for(std::chrono::milliseconds(4)); // ~240 FPS
 	}
 
+	// Cleanup GLFW resources.
 	glfwDestroyWindow(window);
 	glfwTerminate();
+
+	// Clean up the desktop window.
+	// Restores the original wallpaper.
 	lumin::Cleanup();
 
 	return 0;
